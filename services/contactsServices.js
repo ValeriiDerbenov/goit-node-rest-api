490c5583a45195e23b7797127ab0322d4f1f7c88
Valerii <vd.alternative@gmail.com>
2024-03-30 10:42:59 +0200

    getOneContact

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -3,7 +3,6 @@ import Contact from "../models/Contact.js";
 export const listContacts = (ownerId) =>
   Contact.find({ owner: ownerId }, "-createdAt -updatedAt");
 
-// export const addContact = async (body) => Contact.create(body);
 export async function addContact(userId, { name, email, phone }) {
   try {
     const newContact = await Contact.create({
@@ -12,7 +11,6 @@ export async function addContact(userId, { name, email, phone }) {
       phone,
       owner: userId,
     });
-
     return {
       ...newContact._doc,
       __v: undefined,
@@ -22,7 +20,55 @@ export async function addContact(userId, { name, email, phone }) {
   }
 }
 
-export const getContactById = (id) => Contact.findById(id);
-export const removeContact = (id) => Contact.findByIdAndDelete(id);
-export const updateContact = (id, body) => Contact.findByIdAndUpdate(id, body);
+export async function getContactById(contactId, owner) {
+  try {
+    const contact = await Contact.findOne({ _id: contactId, owner });
+
+    if (!contact) {
+      return null;
+    } else {
+      return contact;
+    }
+  } catch (error) {
+    console.error("No contact found.", error);
+  }
+}
+
+export async function removeContact(contactId, owner) {
+  try {
+    const contact = await Contact.findOne({
+      _id: contactId,
+      owner,
+    });
+
+    if (!contact) {
+      return null;
+    }
+    await Contact.findByIdAndDelete(contactId);
+    return contact;
+  } catch (error) {
+    console.error(null);
+  }
+}
+
+export async function updateContact(id, contact_data) {
+  try {
+    const contact = await Contact.findById(id);
+
+    if (!contact) {
+      return null;
+    }
+    const updated_contact = await Contact.findOneAndUpdate(
+      { _id: id },
+      contact_data,
+      {
+        new: true,
+      }
+    );
+    return updated_contact;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
 export const updateStatus = (id, body) => Contact.findByIdAndUpdate(id, body);
