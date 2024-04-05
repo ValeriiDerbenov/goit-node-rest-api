dbfaf7113e1fd6c62bb70ec86343c32d669341d9
Valerii <vd.alternative@gmail.com>
2024-04-05 15:49:11 +0300

    avatar1

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -1,26 +1,42 @@
 import express from "express";
-import 
-  conctactsControllers
- from "../controllers/contactsControllers.js";
-import { createContactSchema, updateContactSchema, updateFavoriteSchema } from "../schemas/contactsSchemas.js";
+import conctactsControllers from "../controllers/contactsControllers.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+  updateFavoriteSchema,
+} from "../schemas/contactsSchemas.js";
 import validateBody from "../helpers/validateBody.js";
-import  isValidId  from '../middlewares/isValidId.js';
-import  authenticate  from "../middlewares/authentication.js";
+import isValidId from "../middlewares/isValidId.js";
+import authenticate from "../middlewares/authentication.js";
 
 const contactsRouter = express.Router();
 
-contactsRouter.use(authenticate)
+contactsRouter.use(authenticate);
 
-contactsRouter.get("/",   conctactsControllers.getAllContacts);
+contactsRouter.get("/", conctactsControllers.getAllContacts);
 
-contactsRouter.get("/:id",   isValidId, conctactsControllers.getOneContact);
+contactsRouter.get("/:id", isValidId, conctactsControllers.getOneContact);
 
-contactsRouter.post("/",   conctactsControllers.createContact, validateBody(createContactSchema));
+contactsRouter.post(
+  "/",
+  conctactsControllers.createContact,
+  validateBody(createContactSchema)
+);
 
-contactsRouter.delete("/:id",   isValidId, conctactsControllers.deleteContact);
+contactsRouter.delete("/:id", isValidId, conctactsControllers.deleteContact);
 
-contactsRouter.put("/:id",  isValidId, conctactsControllers.updateContact, validateBody(updateContactSchema));
+contactsRouter.put(
+  "/:id",
+  isValidId,
+  conctactsControllers.updateContact,
+  validateBody(updateContactSchema)
+);
 
-contactsRouter.patch("/:id/favorite",  isValidId, conctactsControllers.updateStatusContact, validateBody(updateFavoriteSchema));
+contactsRouter.patch(
+  "/:id/favorite",
+  isValidId,
+  conctactsControllers.updateStatusContact,
+  validateBody(updateFavoriteSchema)
+);
 
 export default contactsRouter;
